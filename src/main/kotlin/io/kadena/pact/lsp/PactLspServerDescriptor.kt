2a0481534ec21bbed34cba9e6c7276db6fb220da
Luke Gareth Ribchester <luke@luke.gr>
2024-01-21 23:20:41 +0100

    feat(lsp): bundle executables for macOS, Linux, and Windows
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- src/main/kotlin/io/kadena/pact/lsp/PactLspServerDescriptor.kt
+++ src/main/kotlin/io/kadena/pact/lsp/PactLspServerDescriptor.kt
@@ -3,14 +3,12 @@ package io.kadena.pact.lsp
 import com.google.gson.JsonObject
 import com.intellij.execution.ExecutionException
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
+import io.kadena.pact.ide.settings.AppSettingsState
 import java.io.File
-import java.nio.file.Paths
 
 class PactLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Pact") {
 
@@ -20,19 +18,19 @@ class PactLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor
         )
     }
 
-    fun getBundledExecutablePath(pluginId: String, executableRelativePath: String): String? {
-        // Get the PluginId object from the plugin ID string
-        val id = PluginId.getId(pluginId)
-
-        // Retrieve the IdeaPluginDescriptor
-        val plugin = PluginManagerCore.getPlugin(id)
-
-        // Get the installation directory of the plugin
-        val pluginPath = plugin?.pluginPath?.toFile()?.absolutePath
-
-        // Construct the full path to the bundled executable
-        return pluginPath?.let { Paths.get(it, executableRelativePath).toString() }
-    }
+//    fun getBundledExecutablePath(pluginId: String, executableRelativePath: String): String? {
+//        // Get the PluginId object from the plugin ID string
+//        val id = PluginId.getId(pluginId)
+//
+//        // Retrieve the IdeaPluginDescriptor
+//        val plugin = PluginManagerCore.getPlugin(id)
+//
+//        // Get the installation directory of the plugin
+//        val pluginPath = plugin?.pluginPath?.toFile()?.absolutePath
+//
+//        // Construct the full path to the bundled executable
+//        return pluginPath?.let { Paths.get(it, executableRelativePath).toString() }
+//    }
 
     fun doesExecutableExist(executablePath: String): Boolean {
         val file = File(executablePath)
@@ -42,13 +40,15 @@ class PactLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor
     override fun isSupportedFile(file: VirtualFile) = file.extension == "pact"
 
     override fun createCommandLine(): GeneralCommandLine {
-        val pactLspPath = getBundledExecutablePath("io.kadena.pact", "language-server/pact-lsp-v0.0.1")
+        // Retrieve the configured Pact Language Server executable path
+        val pactLanguageServerPath = AppSettingsState.instance.pactLanguageServerPath
 
-        if (pactLspPath == null || !doesExecutableExist(pactLspPath)) {
-            throw ExecutionException("Pact language server executable not found")
+        if (pactLanguageServerPath == "" || !doesExecutableExist(pactLanguageServerPath)) {
+            throw ExecutionException("Pact Language Server (LSP) executable not found")
         }
 
-        return GeneralCommandLine(pactLspPath).apply {
+        // Start the Pact Language Server
+        return GeneralCommandLine(pactLanguageServerPath).apply {
             withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
             withCharset(Charsets.UTF_8)
             addParameter("--stdio")
@@ -56,11 +56,14 @@ class PactLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor
     }
 
     override fun createInitializationOptions(): String {
-        val pactExecutable = JsonObject()
-        pactExecutable.addProperty("pactExe", "/opt/homebrew/Cellar/pact/4.10.0/bin/pact")
+        // Retrieve the configured Pact executable path
+        val pactPath = AppSettingsState.instance.pactPath
+
+        val pactExe = JsonObject()
+        pactExe.addProperty("pactExe", pactPath)
 
         val options = JsonObject()
-        options.add("pact", pactExecutable)
+        options.add("pact", pactExe)
 
         return options.toString()
     }
