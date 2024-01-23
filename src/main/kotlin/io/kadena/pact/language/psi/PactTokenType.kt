ec28e0a391600777153277d6c7503481a70fac6e
Luke Gareth Ribchester <luke@luke.gr>
2024-01-23 21:17:00 +0100

    feat(lang): implement `PactTokenType`
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/main/kotlin/io/kadena/pact/language/psi/PactTokenType.kt
@@ -0,0 +1,15 @@
+package io.kadena.pact.language.psi
+
+import com.intellij.psi.tree.IElementType
+import io.kadena.pact.language.PactLanguage
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.NotNull
+
+
+class PactTokenType(@NotNull @NonNls debugName: String?) :
+    IElementType(debugName.toString(), PactLanguage.INSTANCE) {
+
+    override fun toString(): String {
+        return "PactTokenType." + super.toString()
+    }
+}
