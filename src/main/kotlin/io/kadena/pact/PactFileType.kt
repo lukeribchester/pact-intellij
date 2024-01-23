7c18ac571005f49f2ba1111243468d9a577bd3c7
Luke Gareth Ribchester <luke@luke.gr>
2024-01-23 20:56:15 +0100

    refactor(lang): update `PactFileType` implementation
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- src/main/kotlin/io/kadena/pact/PactFileType.kt
+++ src/main/kotlin/io/kadena/pact/PactFileType.kt
@@ -1,28 +1,33 @@
 package io.kadena.pact
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
 import javax.swing.Icon
 
-class PactFileType : LanguageFileType(PactLanguage.INSTANCE) {
 
-    companion object {
-        @JvmStatic
-        val INSTANCE = PactFileType()
-    }
-
+class PactFileType private constructor() : LanguageFileType(PactLanguage.INSTANCE) {
+    @NotNull
     override fun getName(): String {
         return "Pact File"
     }
 
+    @NotNull
     override fun getDescription(): String {
         return "Pact smart contract file"
     }
 
+    @NotNull
     override fun getDefaultExtension(): String {
         return "pact"
     }
 
+    @Nullable
     override fun getIcon(): Icon {
         return PactIcons.FILE
+    }
+
+    companion object {
+        val INSTANCE: PactFileType = PactFileType()
     }
 }
