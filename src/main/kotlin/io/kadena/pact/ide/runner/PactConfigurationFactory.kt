5d3cf062d3fd4040b8d1942948cb0f2d00835e4e
Luke Gareth Ribchester <luke@luke.gr>
2024-05-11 04:12:49 +0200

    wip(runner): implement run configuration
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- src/main/kotlin/io/kadena/pact/ide/runner/PactConfigurationFactory.kt
+++ src/main/kotlin/io/kadena/pact/ide/runner/PactConfigurationFactory.kt
@@ -3,19 +3,21 @@ package io.kadena.pact.ide.runner
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.openapi.components.BaseState
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.NotNull
-import org.jetbrains.annotations.Nullable
 
 
 class PactConfigurationFactory constructor(type: ConfigurationType) : ConfigurationFactory(type) {
-    @get:NotNull
-    val id: String
-        get() = PactRunConfigurationType.ID
+    @NotNull
+    override fun getId(): String {
+        return PactRunConfigurationType.ID
+    }
 
-    @get:Nullable
-    val optionsClass: Class<out Any?>
-        get() = PactRunConfigurationOptions::class.java
+    @NotNull
+    override fun getOptionsClass(): Class<out BaseState> {
+        return PactRunConfigurationOptions::class.java
+    }
 
     @NotNull
     override fun createTemplateConfiguration(
--- src/main/kotlin/io/kadena/pact/ide/runner/PactRunConfigurationType.kt
+++ src/main/kotlin/io/kadena/pact/ide/runner/PactRunConfigurationType.kt
@@ -1,13 +1,13 @@
 package io.kadena.pact.ide.runner
 
 import com.intellij.execution.configurations.ConfigurationTypeBase
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.NotNullLazyValue
+import io.kadena.pact.PactIcons
 
 
 internal class PactRunConfigurationType : ConfigurationTypeBase(
     ID, "Pact", "Pact run configuration type",
-    NotNullLazyValue.createValue { AllIcons.Nodes.Console }
+    NotNullLazyValue.createValue { PactIcons.FILE }
 ) {
     init {
         addFactory(PactConfigurationFactory(this))
