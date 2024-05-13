64ecaf4951a0aad0925ba51bf165d0e919684fa3
Luke Gareth Ribchester <luke@luke.gr>
2024-05-13 19:15:24 +0200

    wip(runner): implement run configuration
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/main/kotlin/io/kadena/pact/ide/runner/PactRunConfigurationProducer.kt
@@ -0,0 +1,34 @@
+package io.kadena.pact.ide.runner
+
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.LazyRunConfigurationProducer
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.openapi.util.Ref
+import com.intellij.psi.PsiElement
+
+
+class PactRunConfigurationProducer : LazyRunConfigurationProducer<PactRunConfiguration>() {
+    override fun getConfigurationFactory(): ConfigurationFactory {
+        return PactConfigurationFactory(PactRunConfigurationType())
+    }
+
+    override fun setupConfigurationFromContext(
+        configuration: PactRunConfiguration,
+        context: ConfigurationContext,
+        sourceElement: Ref<PsiElement>
+    ): Boolean {
+        val file = context.psiLocation?.containingFile
+
+        return file?.virtualFile?.extension == "pact"
+    }
+
+    override fun isConfigurationFromContext(
+        configuration: PactRunConfiguration,
+        context: ConfigurationContext
+    ): Boolean {
+        val file = context.psiLocation?.containingFile
+
+        return file?.virtualFile?.extension == "pact" &&
+                configuration.modulePath == file.virtualFile.path
+    }
+}
